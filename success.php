91e0d47f327152b04f396ee7611313cb57eecdd7
guillaumecolombel1@gmail.com <57393436+Nifooo@users.noreply.github.com>
2020-01-17 14:33:18 +0100

    coriger faille contact

--- success.php
+++ success.php
@@ -1,7 +1,4 @@
-<?php
-include('inc/header.php');?>
+<p class="mouv">Votre message à bien été envoyé. <a href="index.php"> Retour à l'accueil</a></p>
 
-    <p class="mouv">Votre message à bien été envoyé. <a href="index.php"> Retour à l'accueil</a></p>
 
-<?php include('inc/footer.php');
 
