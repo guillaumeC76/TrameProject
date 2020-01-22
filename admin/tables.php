71d34fc1b75371536eb95e7e1f10948740e32fc1
samuelLacaille <samuel76560@outlook.fr>
2020-01-22 13:57:21 +0100

    -correction requete d'ajout d'un utilisateur dans l'admin -changement de couleur des titres

--- admin/tables.php
+++ admin/tables.php
@@ -4,10 +4,9 @@ include_once '../inc/pdo.php';
 include_once '../inc/function.php';
 
 
-if($_SESSION['login']['role'] != "admin"){
+if ($_SESSION['login']['role'] != "admin") {
     header('Location: ../404.php');
 }
-
 
 
 $sql = "SELECT * FROM users";
@@ -88,16 +87,15 @@ if (!empty($_POST['submitted'])) {
         $token = generatorToken(120);
 
         // Insert
-        $sql = "INSERT INTO users VALUES (null,:pseudo,:email,:password,:token,'abonne',NOW())";
+        $sql = "INSERT INTO users VALUES (null,:pseudo,:email,:password,:token,:role,NOW())";
         $query = $pdo->prepare($sql);
         $query->bindValue(':pseudo', $pseudo, PDO::PARAM_STR);
         $query->bindValue(':email', $email, PDO::PARAM_STR);
         $query->bindValue(':password', $hashPassword, PDO::PARAM_STR);
         $query->bindValue(':token', $token, PDO::PARAM_STR);
+        $query->bindValue(':role', $role, PDO::PARAM_STR);
         $query->execute();
         $success = true;
-
-
 
 
         header('Location: tables.php');
@@ -105,27 +103,45 @@ if (!empty($_POST['submitted'])) {
 }
 
 if (!empty($_POST['deleteUser'])) {
-    $i=0;
+    $i = 0;
     $id = $user[$i]['id'];
     $sql = "DELETE FROM users WHERE  id = $id ";
     $query = $pdo->prepare($sql);
     $query->execute();
 
 
-
 }
 
 
 if (!empty($_POST['delete'])) {
-    $i = 0 ;
+    $i = 0;
     $id = $contact[$i]['id'];
-    $sql = "DELETE FROM contact WHERE  id = $id ";
+    $sql = "DELETE FROM contact WHERE  id = '" . $id . "' ";
     $query = $pdo->prepare($sql);
+    $query->execute();
+}
+
+
+if (!empty($_POST['modif'])) {
+
+//UPDATE `users` SET `role` = 'admin' WHERE `users`.`id` = 7;
+
+    $i = 0;
+
+    $sql = "UPDATE users SET pseudo = '" . $user[$i]['pseudo'] . "', 
+                             email  = '" . $user[$i]['email'] . "' , 
+                             role  =  '" . $user[$i]['role'] . "' 
+                         WHERE id =   '" . $user[$i]['id'] . "'";
+    $query = $pdo->prepare($sql);
+    $query->bindValue(':pseudo', $user[$i]['pseudo'], PDO::PARAM_STR);
+    $query->bindValue(':email', $user[$i]['email'], PDO::PARAM_STR);
+    $query->bindValue(':role', $user[$i]['role'], PDO::PARAM_STR);
     $query->execute();
 
 
 
-   }
+}
+
 
 ?>
 
@@ -174,8 +190,6 @@ if (!empty($_POST['delete'])) {
         </a>
 
 
-
-
         <!-- Nav Item - Tables -->
         <li class="nav-item active">
             <a class="nav-link" href="tables.php">
@@ -213,8 +227,6 @@ if (!empty($_POST['delete'])) {
 
                 <!-- Topbar Navbar -->
                 <ul class="navbar-nav ml-auto">
-
-
 
 
                     <div class="topbar-divider d-none d-sm-block"></div>
@@ -273,16 +285,18 @@ if (!empty($_POST['delete'])) {
                                 <tbody>
                                 <?php
                                 echo '<tr>'; ?>
-                                <form action="tables.php"  method="post"> <?php
-                                for ($i = 0; $i < count($user); $i++) {
+                                <form action="tables.php" method="post"> <?php
+                                    for ($i = 0;
+                                    $i < count($user);
+                                    $i++) {
                                     echo '<td>' . $user[$i]['id'] . '</td>';
                                     echo '<td>' . $user[$i]['pseudo'] . '</td>';
                                     echo '<td>' . $user[$i]['email'] . '</td>';
                                     echo '<td>' . $user[$i]['role'] . '</td>';
                                     echo '<td>' . $user[$i]['created_at'] . '</td>';
-                                    echo '<td>' . '<input type="submit" name="deleteUser" id="' . $user[$i]['id'] . '" value="supprimer ' . $user[$i]['id'] . '">'.'</td>'; ?>
-                                    </form> <?php
-                                    echo '</tr>';
+                                    echo '<td>' . '<input type="submit" name="deleteUser" id="' . $user[$i]['id'] . '" value="supprimer">' . '</td>'; ?>
+                                </form> <?php
+                                echo '</tr>';
                                 }
                                 ?>
                                 </tbody>
@@ -318,115 +332,154 @@ if (!empty($_POST['delete'])) {
 
                                     </tbody>
                                 </table>
-                                <h3>Modifier un utilisateur :</h3>
 
-                        </div>
-                    </div>
-                    <div class="card shadow mb-4">
-                        <div class="card-header py-3">
-                            <h6 class="m-0 font-weight-bold text-primary">Table contact</h6>
-                        </div>
-                        <div class="card-body">
+
+                            </div>
+                            <h3>Modifier un utilisateur :</h3>
+
                             <div class="table-responsive">
-                                <table class="table table-bordered" id="dataTable4" width="100%" cellspacing="0">
+                                <table class="table table-bordered" id="dataTable3" width="100%" cellspacing="0">
                                     <thead>
                                     <tr>
                                         <th>id</th>
-                                        <th>nom</th>
-                                        <th>prenom</th>
+                                        <th>pseudo</th>
                                         <th>email</th>
-                                        <th>objet</th>
-                                        <th>message</th>
+                                        <th>role</th>
 
                                     </tr>
                                     </thead>
 
                                     <tbody>
-                                    <?php
-                                    echo '<tr>'; ?>
-                                    <form action="tables.php" name="deleteLine" method="post"> <?php
-                                    for ($i = 0; $i < count($contact); $i++) {
-                                        echo '<td>' . $contact[$i]['id'] . '</td>';
-                                        echo '<td>' . $contact[$i]['nom'] . '</td>';
-                                        echo '<td>' . $contact[$i]['prenom'] . '</td>';
-                                        echo '<td>' . $contact[$i]['email'] . '</td>';
-                                        echo '<td>' . $contact[$i]['objet'] . '</td>';
-                                        echo '<td>' . $contact[$i]['message'] . '</td>';
-                                        echo '<td>' . '<input type="submit" name="delete" id="' . $contact[$i]['id'] . '" value="supprimer ' . $contact[$i]['id'] . '">'.'</td>';
-                                      //  echo '<td><a href="tables.php?id=' . $contact[$i]['id'] .'">' . '<img src="img/delete.png"' .'</a>' .'</td>'; ?>
-                                    </form> <?php
-                                        echo '</tr>';
-                                     // debug($contact[$i]['id']);
-                                    }
-                                    ?>
+
+                                    <tr>
+                                        <form action="tables.php" name="modif" method="post">
+                                            <?php for ($i = 0;
+                                            $i < count($user);
+                                            $i++) { ?>
+                                            <td><?php echo $user[$i]['id'] ?></td>
+                                            <td><input type="text" name="pseudoModif"
+                                                       value="<?php echo $user[$i]['pseudo'] ?>"></td>
+                                            <td><input type="text" name="emailModif"
+                                                       value="<?php echo $user[$i]['email'] ?>"></td>
+                                            <td><input type="text" name="roleModif"
+                                                       value="<?php echo $user[$i]['role'] ?>"></td>
+                                            <td><input type="submit" name="modif" value="Modifier"></td>
+                                        </form>
+                                    </tr>
+                                    <?php } ?>
                                     </tbody>
                                 </table>
+
+
+                            </div>
+                        </div>
+                        <div class="card shadow mb-4">
+                            <div class="card-header py-3">
+                                <h6 class="m-0 font-weight-bold text-primary">Table contact</h6>
+                            </div>
+                            <div class="card-body">
+                                <div class="table-responsive">
+                                    <table class="table table-bordered" id="dataTable4" width="100%" cellspacing="0">
+                                        <thead>
+                                        <tr>
+                                            <th>id</th>
+                                            <th>nom</th>
+                                            <th>prenom</th>
+                                            <th>email</th>
+                                            <th>objet</th>
+                                            <th>message</th>
+
+                                        </tr>
+                                        </thead>
+
+                                        <tbody>
+                                        <?php
+                                        echo '<tr>'; ?>
+                                        <form action="tables.php" name="deleteLine" method="post"> <?php
+                                            for ($i = 0;
+                                            $i < count($contact);
+                                            $i++) {
+                                            echo '<td>' . $contact[$i]['id'] . '</td>';
+                                            echo '<td>' . $contact[$i]['nom'] . '</td>';
+                                            echo '<td>' . $contact[$i]['prenom'] . '</td>';
+                                            echo '<td>' . $contact[$i]['email'] . '</td>';
+                                            echo '<td>' . $contact[$i]['objet'] . '</td>';
+                                            echo '<td>' . $contact[$i]['message'] . '</td>';
+                                            echo '<td>' . '<input type="submit" name="delete" id="' . $contact[$i]['id'] . '" value="supprimer ">' . '</td>';
+                                            //  echo '<td><a href="tables.php?id=' . $contact[$i]['id'] .'">' . '<img src="img/delete.png"' .'</a>' .'</td>'; ?>
+                                        </form> <?php
+                                        echo '</tr>';
+                                        // debug($contact[$i]['id']);
+                                        }
+                                        ?>
+                                        </tbody>
+                                    </table>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <!-- /.container-fluid -->
+
                 </div>
-                <!-- /.container-fluid -->
+                <!-- End of Main Content -->
+
+                <!-- Footer -->
+                <footer class="sticky-footer bg-white">
+                    <div class="container my-auto">
+                        <div class="copyright text-center my-auto">
+                            <span>Copyright &copy; TrameProyect 2020</span>
+                        </div>
+                    </div>
+                </footer>
+                <!-- End of Footer -->
 
             </div>
-            <!-- End of Main Content -->
+            <!-- End of Content Wrapper -->
 
-            <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; TrameProyect 2020</span>
+        </div>
+        <!-- End of Page Wrapper -->
+
+        <!-- Scroll to Top Button-->
+        <a class="scroll-to-top rounded" href="#page-top">
+            <i class="fas fa-angle-up"></i>
+        </a>
+
+        <!-- Logout Modal-->
+        <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+             aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel"> Administrateur </h5>
+                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">×</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">Etes-vous sur de vouloir quitter cette page ?</div>
+                    <div class="modal-footer">
+                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Annuler</button>
+                        <a class="btn btn-primary" href="../index.php">retour au site</a>
                     </div>
                 </div>
-            </footer>
-            <!-- End of Footer -->
-
-        </div>
-        <!-- End of Content Wrapper -->
-
-    </div>
-    <!-- End of Page Wrapper -->
-
-    <!-- Scroll to Top Button-->
-    <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
-    </a>
-
-    <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-         aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel"> Administrateur </h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">Etes-vous sur de vouloir quitter cette page ?</div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Annuler</button>
-                    <a class="btn btn-primary" href="../index.php">retour au site</a>
-                </div>
             </div>
         </div>
-    </div>
 
-    <!-- Bootstrap core JavaScript-->
-    <script src="vendor/jquery/jquery.min.js"></script>
-    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+        <!-- Bootstrap core JavaScript-->
+        <script src="vendor/jquery/jquery.min.js"></script>
+        <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-    <!-- Core plugin JavaScript-->
-    <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
+        <!-- Core plugin JavaScript-->
+        <script src="vendor/jquery-easing/jquery.easing.min.js"></script>
 
-    <!-- Custom scripts for all pages-->
-    <script src="js/sb-admin-2.min.js"></script>
+        <!-- Custom scripts for all pages-->
+        <script src="js/sb-admin-2.min.js"></script>
 
-    <!-- Page level plugins -->
-    <script src="vendor/datatables/jquery.dataTables.min.js"></script>
-    <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
+        <!-- Page level plugins -->
+        <script src="vendor/datatables/jquery.dataTables.min.js"></script>
+        <script src="vendor/datatables/dataTables.bootstrap4.min.js"></script>
 
-    <!-- Page level custom scripts -->
-    <script src="js/demo/datatables-demo.js"></script>
+        <!-- Page level custom scripts -->
+        <script src="js/demo/datatables-demo.js"></script>
 
 </body>
 
