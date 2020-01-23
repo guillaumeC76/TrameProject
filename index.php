953cf7e535c6d243aac9426cd409429a2c2ccfa5
guillaumecolombel1@gmail.com <57393436+Nifooo@users.noreply.github.com>
2020-01-23 11:19:47 +0100

    FULL RESPONSIVE POUR LE HOME

--- index.php
+++ index.php
@@ -153,7 +153,6 @@ include('inc/header.php'); ?>
     </div>
 
     <script src="https://code.jquery.com/jquery-2.2.4.min.js" integrity="sha256-BbhdlvQf/xTY9gja0Dq3HiwQF8LaCRTXxZKRutelT44=" crossorigin="anonymous"></script>
-    <script src="asset/flexslider/jquery.flexslider.js"></script>
     <script src="asset/js/main.js"></script>
 
 
