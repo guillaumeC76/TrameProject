cc0e772ced05de3369039551048c7150a20a9646
samuelLacaille <samuel76560@outlook.fr>
2020-01-24 10:35:32 +0100

    fix forgetpassword

--- forgetpassword.php
+++ forgetpassword.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
-include ('inc/function.php');
-include ('inc/pdo.php');
+include('inc/function.php');
+include('inc/pdo.php');
 $errors = array();
 $success = false;
 
@@ -12,20 +12,19 @@ if (!empty($_POST['submitted'])) {
 
     $sql = "SELECT email, token FROM users where email = :email";
     $query = $pdo->prepare($sql);
-    $query->bindValue(':email',$email,PDO::PARAM_STR);
+    $query->bindValue(':email', $email, PDO::PARAM_STR);
     $query->execute();
     $user = $query->fetch();
 
     if (!empty($user)) {
         $token = $user['token'];
         $email = urlencode($email);
-        $html = '<p class="ici"><a href="modif-password.php?token='.$token.'&email='.$email.'">C\'est ici</a></p>';
+        $html = '<p class="ici"><a href="modif-password.php?token=' . $token . '&email=' . $email . '">C\'est ici</a></p>';
         echo $html;
 
     } else {
         $errors['email'] = 'Veuillez renseigner un mot de passe';
     }
-
 
 
 }
@@ -34,19 +33,22 @@ if (!empty($_POST['submitted'])) {
 include('inc/header.php'); ?>
 
 
-<h3 class="headline">Changer de mot de passe</h3>
+    <h3 class="headline">Changer de mot de passe</h3>
 
     <form action="" method="post" class="formpage">
 
         <div class="separation3">
-        <label for="email">Email *</label>
-        <input type="email" name="email" id="email" value="<?php if (!empty($_POST['email'])) { echo $_POST['email'];}?>">
-        <p class="error"><?php if (!empty($errors['email'])) { echo $errors['email']; } ?></p>
+            <label for="email">Email *</label>
+            <input type="email" name="email" id="email" value="<?php if (!empty($_POST['email'])) {
+                echo $_POST['email'];
+            } ?>">
+            <p class="error"><?php if (!empty($errors['email'])) {
+                    echo $errors['email'];
+                } ?></p>
         </div>
 
         <input type="submit" name="submitted" id="boutonenvoyer" value="Modifier le mot de passe">
     </form>
 
 
-
-<?php include ('inc/footer.php');
+<?php include('inc/footer.php');
